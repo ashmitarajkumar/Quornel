fa4f27e6a62c9a1527ef634922a123f3eafff806
cmanage1 <chethinlm@gmail.com>
2020-06-18 16:34:09 -0400

    Initialized Firebase

--- Quornel/Quornel/AppDelegate.swift
+++ Quornel/Quornel/AppDelegate.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -32,6 +35,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
