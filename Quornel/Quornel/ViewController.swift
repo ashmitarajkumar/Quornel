0b34e962e6b1f5fcfa0b0371b3589214e867594f
Ashmita Rajkumar <ashmitarajkumar@gmail.com>
2020-06-18 12:01:42 -0700

    Add files via upload

---
+++ Quornel/Quornel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quornel
+//
+//  Created by Ashmita Rajkumar on 6/18/20.
+//  Copyright © 2020 Ashmita Rajkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
