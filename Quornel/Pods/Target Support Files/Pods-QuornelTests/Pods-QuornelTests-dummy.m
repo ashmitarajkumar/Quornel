ef2fb3905e8eb1a17ac85e168c0f587f521e3b47
cmanage1 <chethinlm@gmail.com>
2020-06-18 16:51:22 -0400

    Add FireBase FireStore

---
+++ Quornel/Pods/Target Support Files/Pods-QuornelTests/Pods-QuornelTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuornelTests : NSObject
+@end
+@implementation PodsDummy_Pods_QuornelTests
+@end
--- Quornel/Quornel/AppDelegate.swift
+++ Quornel/Quornel/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let db = Firestore.firestore()
         return true
     }
 
